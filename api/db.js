9f43ee901038338be28643a674544f617f565ba1
gitakbarhub <akbarashraf373@gmail.com>
2026-01-07 22:36:53 +0500

    Update db.js

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   // PASTE YOUR FULL NEON STRING INSIDE THE QUOTES BELOW:
-  connectionString: "postgresql://neondb_owner:npg_ozpwXvDtfh43@ep-gentle-pond-a4fh43a5-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: "postgresql://neondb_owner:npg_pIbf8HD1yGaj@ep-rapid-star-a4v0qs89-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   ssl: { rejectUnauthorized: false }
 });
 
